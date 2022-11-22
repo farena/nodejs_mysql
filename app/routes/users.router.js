094fa358eef59aa8632a63bb1a305800e54dccd4
farena <fedarena@gmail.com>
2022-11-22 09:53:35 -0300

    lint fix

--- app/routes/users.router.js
+++ app/routes/users.router.js
@@ -5,7 +5,7 @@ const controllers = require('../controllers/index');
 const { authMiddleware } = require('../middlewares/auth.js');
 
 router.post('/login', [], controllers.usersController.login);
-router.put('/profile', [ authMiddleware ], controllers.usersController.updateProfile);
+router.put('/profile', [authMiddleware], controllers.usersController.updateProfile);
 
 module.exports = {
   basePath: '/',
